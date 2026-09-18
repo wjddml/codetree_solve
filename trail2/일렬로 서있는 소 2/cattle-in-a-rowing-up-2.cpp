0eef6b40d21e34921066ff88fced803c2d8071e3
spectatar <jchan3214@naver.com>
2026-09-18 16:03:08 +0900

    [Passed] 일렬로 서있는 소 2 (2ms, 0MB)

---
+++ trail2/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int A[100];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < N - 2; ++i) {
+        for (int j = i + 1; j < N - 1; ++j) {
+            for (int k = j + 1; k < N; ++k) {
+                if (A[i] <= A[j] && A[j] <= A[k]) {
+                    cnt++;
+                }
+            }
+        }
+   }
+
+   cout << cnt << "\n";
+
+    return 0;
+}
